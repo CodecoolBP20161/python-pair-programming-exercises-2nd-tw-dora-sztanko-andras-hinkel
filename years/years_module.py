f55afd4646936db509d0fc114c9a98a0a708bb80
dsztanko <szt.dori@gmail.com>
2016-05-23 13:40:03 +0200

    added extras

--- years/years_module.py
+++ years/years_module.py
@@ -1,16 +1,28 @@
 from datetime import date
 
+
 def years(age):
     years_till_100 = 100 - age
     return date.today().year + years_till_100
 
 
 def main():
+    age = None
+    number = None
     name = input("What is your name?: ")
-    age = int(input("How old are you? "))
+    while age is None:
+        try:
+            age = int(input("How old are you? "))
+        except ValueError:
+            print("Please insert a number for your age!")
+    while number is None:
+        try:
+            number = int(input("How many times should I print this stuff? "))
+        except ValueError:
+            print("Please insert a number!")
     message = "So, {}, you're going to be 100 years old in {}!".format(name, years(age))
-    print(message)
-
+    for i in range(number):
+        print(message)
 
 if __name__ == '__main__':
     main()
