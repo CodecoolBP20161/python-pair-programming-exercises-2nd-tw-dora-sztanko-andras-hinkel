e2b663e55e06d724ff450c9ffcc095d2341524bb
dsztanko <szt.dori@gmail.com>
2016-05-24 10:00:43 +0200

    final version of listoverlap

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,9 +1,20 @@
+import random
+
+
 def listoverlap(list1, list2):
-    return
+    return list(set(list1) & set(list2))
 
 
 def main():
-    return
+    # a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    # b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+    a = [random.randint(1, 50) for x in range(1, random.randint(10, 20))]
+    print(sorted(a))
+    b = [random.randint(1, 50) for x in range(1, random.randint(10, 20))]
+    print(sorted(b))
+
+    print(listoverlap(a, b))
 
 
 if __name__ == '__main__':
