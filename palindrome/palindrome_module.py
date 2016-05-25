166fcb49d8512ad49ba42995a83226304b165232
dsztanko <szt.dori@gmail.com>
2016-05-25 09:25:35 +0200

    Final version of palindrome_module.py

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,16 @@
-def palindrome(str):
-    return
+def palindrome(string):
+    string = string.replace(" ", "").lower()
+    if string == string[::-1]:
+        return True
+    return False
 
 
 def main():
-    return
+    user_input = input("Please type in a string! ")
+    if palindrome(user_input):
+        print("Yeah, this is a palindrome!")
+    else:
+        print("Sorry buddy, it's not a palindrome!")
 
 
 if __name__ == '__main__':
